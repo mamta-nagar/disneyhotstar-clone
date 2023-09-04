54d616339539bd2c92dfb34e0f1fff745499146f
mamta-nagar <143920248+mamta-nagar@users.noreply.github.com>
2023-09-04 07:24:09 +0530

    upload files

---
+++ index.js
@@ -0,0 +1,121 @@
+let movies = [
+    {
+    name:"falcon and the winter soldier",
+    des:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, laborum tenetur blanditiis ipsum optio" ,
+    image:"images/slider 2.png"
+},
+{
+    name:"wanda vision",
+    des: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, laborum tenetur blanditiis ipsum optio ?",
+    image:"images/slider 3.png"
+},
+{
+    name:"falcon and the winter soldier",
+    des:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, laborum tenetur blanditiis ipsum optio ?",
+    image:"images/slider 5.png"
+},
+{
+    name:"raya and the last dragon",
+    des:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, laborum tenetur blanditiis ipsum optio ",
+    image:"images/slider 4.png"
+},
+{
+    name:"wanda vision",
+    des:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, laborum tenetur blanditiis ipsum optio ",
+    image:"images/slider 3.png"
+},
+{
+    name:"loki",
+    des:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, laborum tenetur blanditiis ipsum optio ?",
+    image:"images/slider 1.png"
+},
+{
+    name:"raya and the last dragon",
+    des: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, laborum tenetur blanditiis ipsum optio ?",
+    image:"images/slider 4.png"
+},
+];
+const carousel=document.querySelector('.carousel');
+let sliders=[];
+let slideIndex=0; //track the current slide
+const createSlide = () => {
+    if(slideIndex>=movies.length){
+        slideIndex=0;
+    }
+    
+        //Create DOM elements
+        let slide=document.createElement('div');
+        var imgElement=document.createElement('img');
+        let content=document.createElement('div');
+        let h1=document.createElement('h1');
+        let p=document.createElement('p');
+
+        //attaching all elements
+        imgElement.appendChild(document.createTextNode(''));
+        h1.appendChild(document.createTextNode(movies[slideIndex].name));
+        p.appendChild(document.createTextNode(movies[slideIndex].des));
+        content.appendChild(h1);
+        content.appendChild(p);
+        slide.appendChild(imgElement);
+        carousel.appendChild(slide);
+
+        //setting up images
+        imgElement.src=movies[slideIndex].image;
+        slideIndex++
+
+
+        //setting elements
+        slide.className="slider";
+        content.className="slide-content";
+        h1.className="movie-title";
+        p.className="movie-des";
+
+        sliders.push(slide);
+
+        if(sliders.length){
+            sliders[0].style.marginLeft=`calc(-${100 * (sliders.length - 2)}% - ${
+                30 * (sliders.length - 2)
+            }px)`;
+        }
+    }
+
+
+for(let i=0; i<3; i++){
+    createSlide();
+
+}
+
+setInterval(() => {
+    createSlide();
+}, 3000);
+
+//video cards
+const videocards=[...document.querySelectorAll('.video-card')];
+videocards.forEach(item =>{
+    item.addEventListener('mouseover',()=>{
+        let video =item.children[1];
+        video.play()
+    })
+    item.addEventListener('mouseleave',()=>{
+        let video=item.children[1];
+        video.pause();
+
+    });
+});
+
+//card Sliders
+let cardContainers =[...document.querySelectorAll('.card-container')];
+let preBtns=[...document.querySelectorAll('.pre-btn')];
+let nxtBtns=[...document.querySelectorAll('.nxt-btn')];
+cardContainers.forEach((item,i)=>{
+    let containerDimensions=item.getBoundingClientRect();
+    let containerWidth=containerDimensions.width;
+    nxtBtns[i].addEventListener('click',()=>{
+        item.scrollLeft += containerWidth-200;
+
+    });
+    preBtns[i].addEventListener('click',()=>{
+        item.scrollLeft -= containerWidth+200;
+
+    });
+});
